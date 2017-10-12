8f818c568a960b11d66867cb06ccd183448d1d7e
Kevin Novak <kevinmnovak@yahoo.com>
2017-10-12 12:17:41 -0400

    "Action Completed" button shows for 5 seconds and then hides

--- NFC-Smart-Home/NFC-Smart-Home/ViewController.swift
+++ NFC-Smart-Home/NFC-Smart-Home/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.actionCompletedLabel.isHidden = true
         
 //        let button = UIButton(type: .system)
 //        button.setTitle("Tap to Begin", for: .normal)
@@ -44,16 +45,23 @@ class ViewController: UIViewController {
                 let id = Int(message)!
                 print("found id: \(id)")
                 self.deviceLabel.text = "Call default action for: \(self.deviceLabel.text!)\n\(message)"
-                self.actionCompletedLabel.text = "Action Completed!"
-            }
+                self.actionCompletedLabel.isHidden = false
+                Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(self.hideActionCompletedLabel), userInfo: nil, repeats: false)
+           }
+        }
+    }
+
+    @objc func didTapReadNFC() {
+        if (self.actionCompletedLabel.isHidden) {
+            print("didTapReadNFC")
+            self.deviceLabel.text = ""
+            helper.onNFCResult = onNFCResult(success:msg:)
+            helper.restartSession()
         }
     }
     
-    @objc func didTapReadNFC() {
-        print("didTapReadNFC")
-        self.deviceLabel.text = ""
-        helper.onNFCResult = onNFCResult(success:msg:)
-        helper.restartSession()
+    @objc func hideActionCompletedLabel() {
+      self.actionCompletedLabel.isHidden = true
     }
     
     override func didReceiveMemoryWarning() {
