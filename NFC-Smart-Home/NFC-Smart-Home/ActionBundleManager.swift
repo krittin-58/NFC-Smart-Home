e686468ad77b5e129d21b41ea27d30f96dcac86d
Kevin Novak <kevinmnovak@yahoo.com>
2017-10-12 14:37:12 -0400

    Fixed bundles

--- NFC-Smart-Home/NFC-Smart-Home/ActionBundleManager.swift
+++ NFC-Smart-Home/NFC-Smart-Home/ActionBundleManager.swift
@@ -17,8 +17,8 @@ class ActionBundleManager {
         let action_3 = Action(url: "http://my-api/turn_on_fan")
         let action_4 = Action(url: "http://my-api/turn_off_fan")
         
-        let action_bundle_1 = ActionBundle(actionBundleId: 1, actions: [action_1, action_2])
-        let action_bundle_2 = ActionBundle(actionBundleId: 2, actions: [action_3, action_4])
+        let action_bundle_1 = ActionBundle(actionBundleId: 1, actions: [action_1, action_3])
+        let action_bundle_2 = ActionBundle(actionBundleId: 2, actions: [action_2, action_4])
         let action_bundle_3 = ActionBundle(actionBundleId: 3, actions: [action_1, action_4])
         
         _actionBundles = [action_bundle_1, action_bundle_2, action_bundle_3]
